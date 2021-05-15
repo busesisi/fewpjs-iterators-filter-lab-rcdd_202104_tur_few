04feaca01171060e078eb5d9a5eae5d56597c6cf
Buse Şentürk <buse.senturk@outlook.com>
2021-05-15 14:41:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function findMatching (driverNames, newString) {
     return driverNames.filter((driver) => { return driver.toLowerCase()  ===  string.toLowerCase()})
 }
 
-function fuzzyMatch(driverNames, string){
+function fuzzyMatch(driverNames, newString){
     return driverNames.filter((driver) => {
         return driver[0] === string[0];
     })
 }
 
-function matchName(driversArr, string){
-    return driversArr.filter((obj) => { return obj.name == string})
+function matchName(driversArr, newString){
+    return driverNames.filter((obj) => { return obj.name == string})
 } 
