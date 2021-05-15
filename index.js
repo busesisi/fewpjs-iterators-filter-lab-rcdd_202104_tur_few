91942752328b85eb1bead5c02371bd1f48fb26f8
Buse Şentürk <buse.senturk@outlook.com>
2021-05-15 14:42:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// Code your solution here
-function findMatching (driverNames, newString) {
-    return driverNames.filter((driver) => { return driver.toLowerCase()  ===  string.toLowerCase()})
+function findMatching (driversArr, string) {
+    return driversArr.filter((driver) => { return driver.toLowerCase()  ===  string.toLowerCase()})
 }
 
-function fuzzyMatch(driverNames, newString){
-    return driverNames.filter((driver) => {
+function fuzzyMatch(driversArr, string){
+    return driversArr.filter((driver) => {
         return driver[0] === string[0];
     })
 }
 
-function matchName(driversArr, newString){
-    return driverNames.filter((obj) => { return obj.name == string})
-}
+function matchName(driversArr, string){
+    return driversArr.filter((obj) => { return obj.name == string})
+} 
