4ad680b26957a63d94de5185de2afaece2266bb3
Buse Şentürk <buse.senturk@outlook.com>
2021-05-15 14:41:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching (driverNames, newString) {
+    return driverNames.filter((driver) => { return driver.toLowerCase()  ===  string.toLowerCase()})
+}
+
+function fuzzyMatch(driverNames, string){
+    return driverNames.filter((driver) => {
+        return driver[0] === string[0];
+    })
+}
+
+function matchName(driversArr, string){
+    return driversArr.filter((obj) => { return obj.name == string})
+} 
