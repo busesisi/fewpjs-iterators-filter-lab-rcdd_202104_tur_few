62aeabd8b691c66e7783a3105ea3067b30939921
Buse Şentürk <buse.senturk@outlook.com>
2021-05-15 14:44:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function findMatching (driversArr, string) {
+    return driversArr.filter((driver) => { return driver.toLowerCase()  ===  string.toLowerCase()})
+}
+
+function fuzzyMatch(driversArr, string){
+    return driversArr.filter((driver) => {
+        return driver[0] === string[0];
+    })
+}
+
+function matchName(driversArr, string){
+    return driversArr.filter((obj) => { return obj.name == string})
+}
